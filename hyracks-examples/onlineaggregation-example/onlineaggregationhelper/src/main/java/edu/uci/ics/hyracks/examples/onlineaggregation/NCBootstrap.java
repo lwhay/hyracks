ef9566e255cad17128e06e2c6d31f6314696771d
tcondie <tcondie@123451ca-8445-de46-9d55-352943316053>
2010-11-30 00:28:43 +0000

    add bootstrap classes
    git-svn-id: https://hyracks.googlecode.com/svn/branches/hyracks_online_aggregation@216 123451ca-8445-de46-9d55-352943316053

---
+++ hyracks-examples/onlineaggregation-example/onlineaggregationhelper/src/main/java/edu/uci/ics/hyracks/examples/onlineaggregation/NCBootstrap.java
@@ -0,0 +1,26 @@
+package edu.uci.ics.hyracks.examples.onlineaggregation;
+
+import edu.uci.ics.hyracks.api.application.IApplicationContext;
+import edu.uci.ics.hyracks.api.application.IBootstrap;
+
+public class NCBootstrap implements IBootstrap {
+
+	@Override
+	public void start() throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void stop() throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setApplicationContext(IApplicationContext appCtx) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
