198c963910a4006d174fe6d4730163382f985967
tcondie <tcondie@123451ca-8445-de46-9d55-352943316053>
2010-12-16 01:45:19 +0000

    state count mapper.
    git-svn-id: https://hyracks.googlecode.com/svn/branches/hyracks_online_aggregation@242 123451ca-8445-de46-9d55-352943316053

---
+++ hyracks-examples/onlineaggregation-example/onlineaggregationhelper/src/main/java/edu/uci/ics/hyracks/examples/onlineaggregation/sample/wordcount/StateCountMapper.java
@@ -0,0 +1,24 @@
+package edu.uci.ics.hyracks.examples.onlineaggregation.sample.wordcount;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class StateCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+    private static final IntWritable ONE = new IntWritable(1);
+
+    private Text word = new Text();
+    
+    static enum BListing{id, name, street, city, state, zip, phone, cat, url, chainid};
+    
+
+    @Override
+    public void map(LongWritable key, Text value, Context ctx) throws IOException, InterruptedException {
+    	String[] tuple = value.toString().split(",");
+    	word.set(tuple[BListing.state.ordinal()]);
+        ctx.write(word, ONE);
+    }
+}
