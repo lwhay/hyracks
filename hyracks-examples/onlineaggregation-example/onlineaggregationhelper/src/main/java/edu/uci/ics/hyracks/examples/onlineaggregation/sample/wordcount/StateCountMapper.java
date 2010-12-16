c13899a8601068e9376c6b81993970ac506f110b
tcondie <tcondie@123451ca-8445-de46-9d55-352943316053>
2010-12-16 02:38:24 +0000

    update
    git-svn-id: https://hyracks.googlecode.com/svn/branches/hyracks_online_aggregation@247 123451ca-8445-de46-9d55-352943316053

--- hyracks-examples/onlineaggregation-example/onlineaggregationhelper/src/main/java/edu/uci/ics/hyracks/examples/onlineaggregation/sample/wordcount/StateCountMapper.java
+++ hyracks-examples/onlineaggregation-example/onlineaggregationhelper/src/main/java/edu/uci/ics/hyracks/examples/onlineaggregation/sample/wordcount/StateCountMapper.java
@@ -1,6 +1,8 @@
 package edu.uci.ics.hyracks.examples.onlineaggregation.sample.wordcount;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
@@ -17,7 +19,13 @@ public class StateCountMapper extends Mapper<LongWritable, Text, Text, IntWritab
 
     @Override
     public void map(LongWritable key, Text value, Context ctx) throws IOException, InterruptedException {
+    	System.err.println("LINE: " + value.toString());
     	String[] tuple = value.toString().split("\t");
+    	for (String v : tuple) {
+    		System.err.print(v + ", ");
+    	}
+    	System.err.println("");
+    	
     	if (tuple.length < BListing.state.ordinal()) {
     		System.err.println("STATE: " + tuple[BListing.state.ordinal()]);
     		word.set(tuple[BListing.state.ordinal()]);
