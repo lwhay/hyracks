35fd4b0f01e05771f301b8d8ebd81c9d845d3980
tcondie <tcondie@123451ca-8445-de46-9d55-352943316053>
2010-12-16 02:16:02 +0000

    update
    git-svn-id: https://hyracks.googlecode.com/svn/branches/hyracks_online_aggregation@243 123451ca-8445-de46-9d55-352943316053

--- hyracks-examples/onlineaggregation-example/onlineaggregationhelper/src/main/java/edu/uci/ics/hyracks/examples/onlineaggregation/sample/wordcount/StateCountMapper.java
+++ hyracks-examples/onlineaggregation-example/onlineaggregationhelper/src/main/java/edu/uci/ics/hyracks/examples/onlineaggregation/sample/wordcount/StateCountMapper.java
@@ -17,8 +17,11 @@ public class StateCountMapper extends Mapper<LongWritable, Text, Text, IntWritab
 
     @Override
     public void map(LongWritable key, Text value, Context ctx) throws IOException, InterruptedException {
+    	System.err.println("VALUE: " + value.toString());
     	String[] tuple = value.toString().split(",");
-    	word.set(tuple[BListing.state.ordinal()]);
-        ctx.write(word, ONE);
+    	if (tuple.length < BListing.state.ordinal()) {
+    		word.set(tuple[BListing.state.ordinal()]);
+    		ctx.write(word, ONE);
+    	}
     }
 }
