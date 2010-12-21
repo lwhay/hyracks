4a9417bd722b10b3844109b29ed3356d17f2028b
vinayakb <vinayakb@123451ca-8445-de46-9d55-352943316053>
2010-12-21 19:53:24 +0000

    Erroneous Artefact by release plugin
    git-svn-id: https://hyracks.googlecode.com/svn/tags/hyracks-0.1.3@278 123451ca-8445-de46-9d55-352943316053

--- hyracks/hyracks-api/src/main/java/edu/uci/ics/hyracks/api/application/INCBootstrap.java
+++
@@ -1,5 +0,0 @@
-package edu.uci.ics.hyracks.api.application;
-
-public interface INCBootstrap extends IBootstrap {
-    public void setApplicationContext(INCApplicationContext appCtx);
-}
--- hyracks/hyracks-cli/src/main/java/edu/uci/ics/hyracks/cli/commands/Command.java
+++
@@ -1,7 +0,0 @@
-package edu.uci.ics.hyracks.cli.commands;
-
-import edu.uci.ics.hyracks.cli.CLI;
-
-public abstract class Command {
-    public abstract void run(CLI cli) throws Exception;
-}
