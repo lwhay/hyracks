4597fd4c5dd5ead592ea19037e26635d0dacaf48
alexander.behm <alexander.behm@123451ca-8445-de46-9d55-352943316053>
2011-01-29 06:53:11 +0000

    Modified tokenizer to be able to emit tokens with type tags.
    git-svn-id: https://hyracks.googlecode.com/svn/branches/hyracks_invindex@321 123451ca-8445-de46-9d55-352943316053

--- hyracks-storage-am-invertedindex/src/main/java/edu/uci/ics/hyracks/storage/am/invertedindex/tokenizers/DelimitedUTF8StringBinaryTokenizer.java
+++ hyracks-storage-am-invertedindex/src/main/java/edu/uci/ics/hyracks/storage/am/invertedindex/tokenizers/DelimitedUTF8StringBinaryTokenizer.java
@@ -26,79 +26,89 @@ import edu.uci.ics.hyracks.storage.am.invertedindex.api.IBinaryTokenizer;
 
 public class DelimitedUTF8StringBinaryTokenizer implements IBinaryTokenizer {
 
-    private static final RecordDescriptor tokenSchema = new RecordDescriptor(
-            new ISerializerDeserializer[] { UTF8StringSerializerDeserializer.INSTANCE });
+	private static final RecordDescriptor tokenSchema = new RecordDescriptor(
+			new ISerializerDeserializer[] { UTF8StringSerializerDeserializer.INSTANCE });
 
-    private final char delimiter;
-    private byte[] data;
-    private int start;
-    private int length;
+	private final char delimiter;
+	private final byte typeTag;
+	private byte[] data;
+	private int start;
+	private int length;
 
-    private int tokenLength;
-    private int tokenStart;
-    private int pos;
+	private int tokenLength;
+	private int tokenStart;
+	private int pos;
 
-    public DelimitedUTF8StringBinaryTokenizer(char delimiter) {
-        this.delimiter = delimiter;
-    }
+	public DelimitedUTF8StringBinaryTokenizer(char delimiter, byte typeTag) {
+		this.delimiter = delimiter;
+		this.typeTag = typeTag;
+	}
+	
+	public DelimitedUTF8StringBinaryTokenizer(char delimiter) {
+		this.delimiter = delimiter;
+		this.typeTag = -1;
+	}
 
-    @Override
-    public int getTokenLength() {
-        return tokenLength;
-    }
+	@Override
+	public int getTokenLength() {
+		return tokenLength;
+	}
 
-    @Override
-    public int getTokenStartOff() {
-        return tokenStart;
-    }
+	@Override
+	public int getTokenStartOff() {
+		return tokenStart;
+	}
 
-    @Override
-    public boolean hasNext() {
-        if (pos >= start + length)
-            return false;
-        else
-            return true;
-    }
+	@Override
+	public boolean hasNext() {
+		if (pos >= start + length)
+			return false;
+		else
+			return true;
+	}
 
-    @Override
-    public void next() {
-        tokenLength = 0;
-        tokenStart = pos;
-        while (pos < start + length) {
-            int len = StringUtils.charSize(data, pos);
-            char ch = StringUtils.charAt(data, pos);
-            pos += len;
-            if (ch == delimiter) {
-                break;
-            }
-            tokenLength += len;
-        }
-    }
+	@Override
+	public void next() {
+		tokenLength = 0;
+		tokenStart = pos;
+		while (pos < start + length) {
+			int len = StringUtils.charSize(data, pos);
+			char ch = StringUtils.charAt(data, pos);
+			pos += len;
+			if (ch == delimiter) {
+				break;
+			}
+			tokenLength += len;
+		}
+	}
 
-    @Override
-    public void reset(byte[] data, int start, int length) {
-        this.data = data;
-        this.start = start;
-        this.pos = start;
-        this.length = length;
-        this.tokenLength = 0;
-        this.tokenStart = 0;
-        pos += 2; // UTF-8 specific
-    }
+	@Override
+	public void reset(byte[] data, int start, int length) {
+		this.data = data;
+		this.start = start;
+		this.pos = start;
+		this.length = length;
+		this.tokenLength = 0;
+		this.tokenStart = 0;
+		pos += 2; // UTF-8 specific
+	}
 
-    @Override
-    public void writeToken(DataOutput dos) throws IOException {
-        // WARNING: 2-byte length indicator is specific to UTF-8
-        dos.writeShort((short) tokenLength);
-        dos.write(data, tokenStart, tokenLength);
-    }
+	@Override
+	public void writeToken(DataOutput dos) throws IOException {
+		if (typeTag > 0)
+			dos.write(typeTag);
 
-    @Override
-    public RecordDescriptor getTokenSchema() {
-        return tokenSchema;
-    }
+		// WARNING: 2-byte length indicator is specific to UTF-8
+		dos.writeShort((short) tokenLength);
+		dos.write(data, tokenStart, tokenLength);
+	}
 
-    // cannot be implemented for this tokenizer
+	@Override
+	public RecordDescriptor getTokenSchema() {
+		return tokenSchema;
+	}
+
+	// cannot be implemented for this tokenizer
 	@Override
 	public int getNumTokens() {
 		return -1;
