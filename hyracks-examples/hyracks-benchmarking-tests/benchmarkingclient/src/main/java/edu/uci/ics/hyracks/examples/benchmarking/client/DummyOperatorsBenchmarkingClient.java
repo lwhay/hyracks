25437d7641480a38d7c640777641c96b35d8d2e7
jarodwen@gmail.com <jarodwen@gmail.com@123451ca-8445-de46-9d55-352943316053>
2011-04-12 22:27:45 +0000

    DummyBenchmarkingTest: Added switches for different kinds of operator connectors.
    git-svn-id: https://hyracks.googlecode.com/svn/branches/hyracks_spilling_groupby_perf@387 123451ca-8445-de46-9d55-352943316053

--- hyracks-examples/hyracks-benchmarking-tests/benchmarkingclient/src/main/java/edu/uci/ics/hyracks/examples/benchmarking/client/DummyOperatorsBenchmarkingClient.java
+++ hyracks-examples/hyracks-benchmarking-tests/benchmarkingclient/src/main/java/edu/uci/ics/hyracks/examples/benchmarking/client/DummyOperatorsBenchmarkingClient.java
@@ -24,17 +24,20 @@ import edu.uci.ics.hyracks.api.client.HyracksRMIConnection;
 import edu.uci.ics.hyracks.api.client.IHyracksClientConnection;
 import edu.uci.ics.hyracks.api.constraints.PartitionConstraintHelper;
 import edu.uci.ics.hyracks.api.dataflow.IConnectorDescriptor;
+import edu.uci.ics.hyracks.api.dataflow.value.IBinaryHashFunctionFactory;
 import edu.uci.ics.hyracks.api.job.JobSpecification;
+import edu.uci.ics.hyracks.dataflow.common.data.partition.FieldHashPartitionComputerFactory;
 import edu.uci.ics.hyracks.dataflow.std.base.AbstractSingleActivityOperatorDescriptor;
 import edu.uci.ics.hyracks.dataflow.std.benchmarking.DummyInOutOperatorDescriptor;
 import edu.uci.ics.hyracks.dataflow.std.benchmarking.DummyInputOperatorDescriptor;
 import edu.uci.ics.hyracks.dataflow.std.benchmarking.DummyInputSinkOperatorDescriptor;
 import edu.uci.ics.hyracks.dataflow.std.benchmarking.DummyOperatorDescriptor;
+import edu.uci.ics.hyracks.dataflow.std.connectors.MToNHashPartitioningConnectorDescriptor;
+import edu.uci.ics.hyracks.dataflow.std.connectors.MToNRangePartitioningConnectorDescriptor;
 import edu.uci.ics.hyracks.dataflow.std.connectors.OneToOneConnectorDescriptor;
 
 /**
  * @author jarodwen
- *
  */
 public class DummyOperatorsBenchmarkingClient {
     private static class Options {
@@ -46,12 +49,15 @@ public class DummyOperatorsBenchmarkingClient {
 
         @Option(name = "-app", usage = "Hyracks Application name", required = true)
         public String app;
-        
+
         @Option(name = "-in-node-splits", usage = "Comma separated list of nodes for the input. A node is <node-name>", required = true)
         public String inNodeSplits;
 
         @Option(name = "-out-node-splits", usage = "Comma separated list of nodes for the output", required = true)
         public String outNodeSplits;
+
+        @Option(name = "-connector-type", usage = "The type of the connector between operators", required = true)
+        public int connectorType;
 
         @Option(name = "-test-count", usage = "Number of runs for benchmarking")
         public int testCount = 3;
@@ -59,30 +65,32 @@ public class DummyOperatorsBenchmarkingClient {
         @Option(name = "-chain-length", usage = "The length of the chain of dummy operators")
         public int chainLength = 1;
     }
-    
+
     private static final Pattern splitPattern = Pattern.compile(",");
-    
+
     /**
      * @param args
-     * @throws Exception 
+     * @throws Exception
      */
     public static void main(String[] args) throws Exception {
-        
+
         Options options = new Options();
         CmdLineParser parser = new CmdLineParser(options);
         parser.parseArgument(args);
-        
+
         IHyracksClientConnection hcc = new HyracksRMIConnection(options.host, options.port);
 
         JobSpecification job;
-        
-        System.out.println("Test information:\n" + "ChainLength\n"
-                + options.chainLength);
-        
+
+        System.out.println("Test information:\n" + "ChainLength\tConnectorType\tInNodeSplits\tOutNodeSplits\n"
+                + options.chainLength + "\t" + options.connectorType + "\t" + options.inNodeSplits + "\t"
+                + options.outNodeSplits);
+
         System.out.println("\tInitial\tRunning");
         for (int i = 0; i < options.testCount; i++) {
             long start = System.currentTimeMillis();
-            job = createJob(options.chainLength, splitPattern.split(options.inNodeSplits), splitPattern.split(options.outNodeSplits));
+            job = createJob(options.chainLength, splitPattern.split(options.inNodeSplits),
+                    splitPattern.split(options.outNodeSplits), options.connectorType);
             System.out.print(i + "\t" + (System.currentTimeMillis() - start));
             start = System.currentTimeMillis();
             UUID jobId = hcc.createJob(options.app, job);
@@ -91,41 +99,58 @@ public class DummyOperatorsBenchmarkingClient {
             System.out.println("\t" + (System.currentTimeMillis() - start));
         }
     }
-    
-    private static JobSpecification createJob(int chainLength, String[] inNodes, String[] outNodes) {
+
+    private static JobSpecification createJob(int chainLength, String[] inNodes, String[] outNodes, int connectorType) {
         JobSpecification spec = new JobSpecification();
 
         // Dummy operator
 
         AbstractSingleActivityOperatorDescriptor source, sink;
-        
-        if(chainLength == 1){
+
+        if (chainLength == 1) {
             source = new DummyOperatorDescriptor(spec);
             PartitionConstraintHelper.addAbsoluteLocationConstraint(spec, source, inNodes);
             sink = source;
-        }else{
+        } else {
             source = new DummyInputOperatorDescriptor(spec);
             PartitionConstraintHelper.addAbsoluteLocationConstraint(spec, source, inNodes);
-            
+
             AbstractSingleActivityOperatorDescriptor opter = source;
-            
-            for(int i = 0; i < chainLength - 2; i++){
+
+            for (int i = 0; i < chainLength - 2; i++) {
                 DummyInOutOperatorDescriptor dummyChain = new DummyInOutOperatorDescriptor(spec);
                 PartitionConstraintHelper.addAbsoluteLocationConstraint(spec, dummyChain, inNodes);
-                IConnectorDescriptor connChain = new OneToOneConnectorDescriptor(spec);
+                IConnectorDescriptor connChain;
+                switch (connectorType) {
+                    case 0:
+                        connChain = new OneToOneConnectorDescriptor(spec);
+                        break;
+                    case 1:
+                        connChain = new MToNHashPartitioningConnectorDescriptor(
+                                spec,
+                                new FieldHashPartitionComputerFactory(new int[] {}, new IBinaryHashFunctionFactory[] {}));
+                        break;
+                    case 2:
+                        connChain = new MToNRangePartitioningConnectorDescriptor(spec, 0, null);
+                        break;
+                    default:
+                        connChain = new OneToOneConnectorDescriptor(spec);
+                        break;
+                }
+
                 spec.connect(connChain, opter, 0, dummyChain, 0);
                 opter = dummyChain;
             }
-            
+
             sink = new DummyInputSinkOperatorDescriptor(spec);
             PartitionConstraintHelper.addAbsoluteLocationConstraint(spec, sink, outNodes);
-            
+
             IConnectorDescriptor conn = new OneToOneConnectorDescriptor(spec);
             spec.connect(conn, opter, 0, sink, 0);
         }
 
         spec.addRoot(sink);
-        
+
         return spec;
     }
 
