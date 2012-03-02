73dc8e5420c9dc394cfb63c1db135845b4df775f
kisskys <kisskys@123451ca-8445-de46-9d55-352943316053>
2012-03-02 08:16:08 +0000

    git-svn-id: https://hyracks.googlecode.com/svn/branches/hyracks_inverted_index_updates@1264 123451ca-8445-de46-9d55-352943316053

--- hyracks-storage-am-lsm-invertedindex/src/main/java/edu/uci/ics/hyracks/storage/am/lsm/invertedindex/impls/LSMInvertedIndex.java
+++ hyracks-storage-am-lsm-invertedindex/src/main/java/edu/uci/ics/hyracks/storage/am/lsm/invertedindex/impls/LSMInvertedIndex.java
@@ -1,5 +1,20 @@
+/*
+ * Copyright 2009-2012 by The Regents of the University of California
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * you may obtain a copy of the License from
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package edu.uci.ics.hyracks.storage.am.lsm.invertedindex.impls;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -12,14 +27,42 @@ import edu.uci.ics.hyracks.storage.am.common.api.IIndexOpContext;
 import edu.uci.ics.hyracks.storage.am.common.api.ISearchPredicate;
 import edu.uci.ics.hyracks.storage.am.common.api.IndexException;
 import edu.uci.ics.hyracks.storage.am.common.api.IndexType;
+import edu.uci.ics.hyracks.storage.am.invertedindex.api.IInvertedIndex;
 import edu.uci.ics.hyracks.storage.am.lsm.common.api.ILSMComponentFinalizer;
+import edu.uci.ics.hyracks.storage.am.lsm.common.api.ILSMFileManager;
 import edu.uci.ics.hyracks.storage.am.lsm.common.api.ILSMIndex;
 import edu.uci.ics.hyracks.storage.am.lsm.common.freepage.InMemoryFreePageManager;
+import edu.uci.ics.hyracks.storage.am.lsm.common.impls.LSMHarness;
+import edu.uci.ics.hyracks.storage.am.lsm.common.impls.TreeIndexComponentFinalizer;
 import edu.uci.ics.hyracks.storage.common.buffercache.IBufferCache;
+import edu.uci.ics.hyracks.storage.common.file.IFileMapProvider;
 
 public class LSMInvertedIndex implements ILSMIndex {
-
-    @Override
+	
+    private final LSMHarness 				lsmHarness;
+    private final IInvertedIndex			memoryBTreeInvertedIndex;
+    private final InvertedIndexFactory 		diskInvertedIndexFactory;
+    private final ILSMFileManager 			fileManager;
+    private final IFileMapProvider 			diskFileMapProvider;
+    private final ILSMComponentFinalizer 	componentFinalizer;
+    private LinkedList<Object> 				diskInvertedIndex = new LinkedList<Object>();
+    
+	public LSMInvertedIndex(
+			IInvertedIndex 			memoryBTreeInvertedIndex,
+			InvertedIndexFactory 	diskInvertedIndexFactory,
+			ILSMFileManager 		fileManager,
+			IFileMapProvider 		diskFileMapProvider	
+	)
+	{
+		this.memoryBTreeInvertedIndex = memoryBTreeInvertedIndex;
+		this.diskInvertedIndexFactory = diskInvertedIndexFactory;
+		this.fileManager = fileManager;
+		this.diskFileMapProvider = diskFileMapProvider;
+		this.lsmHarness = new LSMHarness(this);
+		this.componentFinalizer = new TreeIndexComponentFinalizer(diskFileMapProvider); 
+	}
+    
+	@Override
     public void create(int indexFileId) throws HyracksDataException {
         // TODO Auto-generated method stub
         
