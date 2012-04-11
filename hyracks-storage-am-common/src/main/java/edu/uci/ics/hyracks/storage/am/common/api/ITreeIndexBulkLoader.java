f1e3cb3c917cd9056fd26bcfb867258ed984a9f3
mdreseler@gmail.com <mdreseler@gmail.com@123451ca-8445-de46-9d55-352943316053>
2012-04-11 01:59:31 +0000

    Added files missing in previous commit
    git-svn-id: https://hyracks.googlecode.com/svn/branches/hyracks_rtree_bulkload@1351 123451ca-8445-de46-9d55-352943316053

---
+++ hyracks-storage-am-common/src/main/java/edu/uci/ics/hyracks/storage/am/common/api/ITreeIndexBulkLoader.java
@@ -0,0 +1,35 @@
+package edu.uci.ics.hyracks.storage.am.common.api;
+
+import edu.uci.ics.hyracks.api.exceptions.HyracksDataException;
+import edu.uci.ics.hyracks.dataflow.common.data.accessors.ITupleReference;
+
+public interface ITreeIndexBulkLoader {
+	// FIXME Doku
+
+    /**
+     * Append a tuple to the index in the context of a bulk load.
+     * 
+     * @param tuple
+     *            Tuple to be inserted.
+     * @param ictx
+     *            Existing bulk load context.
+     * @throws HyracksDataException
+     *             If the BufferCache throws while un/pinning or un/latching.
+     * @throws PageAllocationException
+     */
+    public void add(ITupleReference tuple)
+            throws HyracksDataException;
+
+    /**
+     * Finalize the bulk loading operation in the given context.
+     * 
+     * @param ictx
+     *            Existing bulk load context to be finalized.
+     * @throws HyracksDataException
+     *             If the BufferCache throws while un/pinning or un/latching.
+     * @throws PageAllocationException
+     */
+    public void end()
+            throws HyracksDataException;
+
+}
