f3213923931ccc444df4afa42adb161c38e8bb42
tcondie <tcondie@123451ca-8445-de46-9d55-352943316053>
2012-05-31 01:44:46 +0000

    update
    git-svn-id: https://hyracks.googlecode.com/svn/branches/hyracks_scalops_integration@1514 123451ca-8445-de46-9d55-352943316053

--- hyracks-scalops/src/main/scala/com/yahoo/scalops/dsl/transformations/MapTransformation.scala
+++ hyracks-scalops/src/main/scala/com/yahoo/scalops/dsl/transformations/MapTransformation.scala
@@ -12,13 +12,24 @@
 package com.yahoo.scalops.dsl.transformations
 
 import com.yahoo.scalops.dsl.Queryable
+import com.yahoo.scalops.algebricks.IClojureEvaluator
+import java.io.DataOutput
+import edu.uci.ics.hyracks.dataflow.common.data.accessors.IFrameTupleReference
+import com.yahoo.scalops.util.Utils
 
 /**
  * Map transformation: Apply a given function to all records in the queryable, yielding a new queryable of the output type.
  */
 case class MapTransformation[T1, T2](val input: Queryable[T1], val f: T1 => T2) extends Transformation[T2] {
   override def toString = input.toString + ".map(" + f.toString + ")"
-}
-
-trait MapTransformationImplicits {
+  
+  class ClosureEvaluator(val f: T1 => T2) extends IClosureEvaluator {
+    override def getName : String = "map" + f.hashCode()
+    
+	override def evaluator(tuple : IFrameTupleReference, output : DataOutput) : Unit = {
+	   val result = f(Utils.deserialize(tuple.getFieldData(0), tuple.getFieldStart(0), tuple.getFieldLength(0)))
+	   output.write(Utils.serialize(result))
+	} 
+  }
+  
 }
