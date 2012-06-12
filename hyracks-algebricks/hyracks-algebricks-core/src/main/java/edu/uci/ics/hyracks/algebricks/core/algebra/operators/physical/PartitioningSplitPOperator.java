32a60550167333ab54d9b5cc0b1636d6068b0a82
alexander.behm <alexander.behm@123451ca-8445-de46-9d55-352943316053>
2012-06-12 23:43:20 +0000

    Finished PartitioningSplitPOperator.
    git-svn-id: https://hyracks.googlecode.com/svn/branches/hyracks_asterix_fuzzy@1550 123451ca-8445-de46-9d55-352943316053

--- hyracks-algebricks/hyracks-algebricks-core/src/main/java/edu/uci/ics/hyracks/algebricks/core/algebra/operators/physical/PartitioningSplitPOperator.java
+++ hyracks-algebricks/hyracks-algebricks-core/src/main/java/edu/uci/ics/hyracks/algebricks/core/algebra/operators/physical/PartitioningSplitPOperator.java
@@ -1,16 +1,25 @@
 package edu.uci.ics.hyracks.algebricks.core.algebra.operators.physical;
 
+import org.apache.commons.lang3.mutable.Mutable;
+
 import edu.uci.ics.hyracks.algebricks.common.exceptions.AlgebricksException;
 import edu.uci.ics.hyracks.algebricks.core.algebra.base.IHyracksJobBuilder;
+import edu.uci.ics.hyracks.algebricks.core.algebra.base.ILogicalExpression;
 import edu.uci.ics.hyracks.algebricks.core.algebra.base.ILogicalOperator;
 import edu.uci.ics.hyracks.algebricks.core.algebra.base.IOptimizationContext;
 import edu.uci.ics.hyracks.algebricks.core.algebra.base.PhysicalOperatorTag;
+import edu.uci.ics.hyracks.algebricks.core.algebra.expressions.ILogicalExpressionJobGen;
 import edu.uci.ics.hyracks.algebricks.core.algebra.operators.logical.AbstractLogicalOperator;
 import edu.uci.ics.hyracks.algebricks.core.algebra.operators.logical.IOperatorSchema;
+import edu.uci.ics.hyracks.algebricks.core.algebra.operators.logical.PartitioningSplitOperator;
 import edu.uci.ics.hyracks.algebricks.core.algebra.properties.IPhysicalPropertiesVector;
 import edu.uci.ics.hyracks.algebricks.core.algebra.properties.PhysicalRequirements;
 import edu.uci.ics.hyracks.algebricks.core.algebra.properties.StructuralPropertiesVector;
 import edu.uci.ics.hyracks.algebricks.core.jobgen.impl.JobGenContext;
+import edu.uci.ics.hyracks.algebricks.core.jobgen.impl.JobGenHelper;
+import edu.uci.ics.hyracks.algebricks.runtime.base.IEvaluatorFactory;
+import edu.uci.ics.hyracks.algebricks.runtime.operators.std.PartitioningSplitOperatorDescriptor;
+import edu.uci.ics.hyracks.api.dataflow.value.RecordDescriptor;
 
 public class PartitioningSplitPOperator extends AbstractPhysicalOperator {
 
@@ -38,10 +47,23 @@ public class PartitioningSplitPOperator extends AbstractPhysicalOperator {
 
     @Override
     public void contributeRuntimeOperator(IHyracksJobBuilder builder, JobGenContext context, ILogicalOperator op,
-            IOperatorSchema propagatedSchema, IOperatorSchema[] inputSchemas, IOperatorSchema outerPlanSchema)
+            IOperatorSchema opSchema, IOperatorSchema[] inputSchemas, IOperatorSchema outerPlanSchema)
             throws AlgebricksException {
-        // TODO Auto-generated method stub
-        
+        PartitioningSplitOperator partSplitOp = (PartitioningSplitOperator) op;
+        Mutable<ILogicalExpression>[] expressions = partSplitOp.getExpressions();
+        IEvaluatorFactory[] evalFactories = new IEvaluatorFactory[expressions.length];
+        ILogicalExpressionJobGen exprJobGen = context.getExpressionJobGen();
+        for (int i = 0; i < evalFactories.length; i++) {
+            evalFactories[i] = exprJobGen.createEvaluatorFactory(expressions[i].getValue(),
+                    context.getTypeEnvironment(op.getInputs().get(0).getValue()), inputSchemas, context);
+        }
+        RecordDescriptor recDesc = JobGenHelper.mkRecordDescriptor(context.getTypeEnvironment(op), opSchema, context);
+        PartitioningSplitOperatorDescriptor partSplitOpDesc = new PartitioningSplitOperatorDescriptor(
+                builder.getJobSpec(), evalFactories, context.getBinaryBooleanInspector(), partSplitOp.hasDefault(),
+                recDesc);
+        contributeOpDesc(builder, partSplitOp, partSplitOpDesc);
+        ILogicalOperator srcExchange = partSplitOp.getInputs().get(0).getValue();
+        builder.contributeGraphEdge(srcExchange, 0, partSplitOp, 0);
     }
 
 }
