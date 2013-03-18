86e58a272e67de6999590cffedb37e69095242c2
cnsdqdwangrui@gmail.com <cnsdqdwangrui@gmail.com@123451ca-8445-de46-9d55-352943316053>
2013-03-18 04:51:55 +0000

    IMRU auto generate key pairs to simplify manual
    git-svn-id: https://hyracks.googlecode.com/svn/branches/fullstack_imru@3141 123451ca-8445-de46-9d55-352943316053

--- imru/imru-dist/src/main/resources/scripts/rsyncAllNCs.sh
+++ imru/imru-dist/src/main/resources/scripts/rsyncAllNCs.sh
@@ -7,14 +7,20 @@
 CUR_DIR=$(cd $(dirname "$0"); pwd)
 APPASSEMBLER_DIR=$(cd $(dirname "$CUR_DIR"); pwd)
 HOME=$(cd ~; pwd)
-PUB_KEY=$(cat $HOME/.ssh/id_rsa.pub)
 
 #Get the IP address of the cc
 CCHOST=`$CUR_DIR/getip.sh`
 
+if test ! "(" -e $HOME/.ssh/id_rsa.pub ")"
+then
+	ssh-keygen -f $HOME/.ssh/id_rsa -N ''
+fi
+
+PUB_KEY=$(cat $HOME/.ssh/id_rsa.pub)
+
 
 for i in `cat $APPASSEMBLER_DIR/conf/slaves`
 do
-	ssh $i "cd ~/.ssh;grep \"$PUB_KEY\" authorized_keys > /dev/null;if test \$? -ne 0;then echo 'add pub key';echo \"$PUB_KEY\" > authorized_keys;else echo 'already has pub key';fi;mkdir -p ${APPASSEMBLER_DIR}"
+	ssh $i "cd ~/.ssh;grep \"$PUB_KEY\" authorized_keys > /dev/null;if test \$? -ne 0;then echo 'add pub key';echo \"$PUB_KEY\" > authorized_keys;chmod 600 authorized_keys;else echo 'already has pub key';fi;mkdir -p ${APPASSEMBLER_DIR}"
 	rsync -vrultzC ${APPASSEMBLER_DIR}/ $i:${APPASSEMBLER_DIR}/
 done
