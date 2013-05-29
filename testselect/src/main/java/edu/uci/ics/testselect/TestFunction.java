77557c9acf84f01e2e4ba05c99b6c99830d599d4
kereno <kereno@gmail.com>
2013-05-28 18:45:32 -0700

    Initial upload of testselect

---
+++ testselect/src/main/java/edu/uci/ics/testselect/TestFunction.java
@@ -0,0 +1,16 @@
+import edu.uci.ics.hyracks.algebricks.core.algebra.functions.FunctionIdentifier;
+import edu.uci.ics.hyracks.algebricks.core.algebra.functions.IFunctionInfo;
+
+
+public class TestFunction implements IFunctionInfo {
+    private final FunctionIdentifier fid;
+
+    public TestFunction(FunctionIdentifier fid) {
+        this.fid = fid;
+    }
+
+    @Override
+    public FunctionIdentifier getFunctionIdentifier() {
+        return fid;
+    }
+}
