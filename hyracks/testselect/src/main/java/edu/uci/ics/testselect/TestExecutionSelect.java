71aba1df350c7b28fd94d2ab5b75932ebd009ab6
kereno <kereno@gmail.com>
2013-06-05 19:21:55 -0700

    adding rules to testselect

---
+++ hyracks/testselect/src/main/java/edu/uci/ics/testselect/TestExecutionSelect.java
@@ -0,0 +1,13 @@
+package edu.uci.ics.testselect;
+
+public class TestExecutionSelect {
+	public static void main(String[] args) {
+		//open each operator
+		
+		//call nextFrame bottom-up on the plan
+		
+		 //you only need to open the first operator, 
+		//and other operators will be opened as then 
+		//are chained in a plan.
+	}
+}
