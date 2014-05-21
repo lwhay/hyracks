b0bc108b67095535e66dfae834de94c96a3a9f33
Till Westmann <westmann@gmail.com>
2014-05-21 15:44:28 -0700

    get all result partitions - fix loop condition - add more logging to HyracksDatasetReader - more consistent toString() for subclasses of AbstractWork

--- hyracks/hyracks-api/src/main/java/edu/uci/ics/hyracks/api/dataset/DatasetDirectoryRecord.java
+++ hyracks/hyracks-api/src/main/java/edu/uci/ics/hyracks/api/dataset/DatasetDirectoryRecord.java
@@ -3,9 +3,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * you may obtain a copy of the License from
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -91,5 +91,10 @@ public class DatasetDirectoryRecord implements Serializable {
             return false;
         }
         return address.equals(((DatasetDirectoryRecord) o).address);
+    }
+
+    @Override
+    public String toString() {
+        return address.toString() + " " + status + (empty ? " (empty)" : "") + (readEOS ? " (EOS)" : "");
     }
 }
